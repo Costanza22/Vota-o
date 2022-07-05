74abc1aca7e7a19a6e1c7ba1c455aa1620632e52
Costanza Assef <81304267+Costanza22@users.noreply.github.com>
2022-07-05 10:10:33 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+
+
+
+private class administradores {
+  
+int CPF;
+  
+string Senhaadministrador;
+  
+public void cadastrarlinguagem();
+
+}
+private class linguagem {
+  string nomelinguagem;
+  int codigolinguagem;
+
+  public void linguagem();
+  
+}
+  private class votante {
+
+    int CPF;
+    string nomevotante;
+    string emailvotante;
+    string senhavotante;
+    string repetirsenhavotante;
+    string votar;
+    
+public void votar() null;
+
+    private class
+    
+} 
